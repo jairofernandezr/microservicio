80d05d8324f2a5a83914f9bd716da4e703e7b259
jairo fernandez <jairofernandezr@gmail.com>
2019-07-23 19:56:42 -0500

    Agregando microservicio

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony', 700, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple', 1000, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Adidas', 600, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Reebok', 300, NOW());
